b51ebc14a8e5def6bb40c53c95e1a1b9ffe3bc12
wangkf <wangkf@gmail.com>
2016-05-20 15:37:13 +0800

    Update start.sh

--- PiBox/start.sh
+++ PiBox/start.sh
@@ -10,29 +10,29 @@ echo '+---+   +----+ |   | | | | |   '
 echo '|     + |    | |   | | | | |   '
 echo '|     | +    + +---+ + + + +--+'
 echo '|     |                        '
-echo '+     +             Hello World'
+echo '+     +            你好，物联网'
 
 ret=`netstat -an |grep 8000`
 if [ "$ret" ];then
-   echo 'A program is already using port 8000 '
+   echo '一个程序已使用 8000 端口'
    exit
 fi
 ret=`netstat -an |grep 8001`
 if [ "$ret" ];then
-   echo 'A program is already using port 8001'
+   echo '一个程序已使用 8001 端口'
    exit
 fi
 ret=`netstat -an |grep 3333`
 if [ "$ret" ];then
-   echo 'A program is already using port 3333'
+   echo '一个程序已使用 3333 端口'
    exit
 fi
 
-echo "Attention : If you can't see anything in the browser, please check log in log/pihome.log"
+echo "注意 : 如果你在浏览器中不能看到任何信息，请检查位于 log/pihome.log 的日志文件"
 
 ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 
-echo "http://$ip:8000  or others"
+echo "http://$ip:8000  或其他"
 
 nohup ./CppClient/pihome > ./log/cpp.log 2>&1 &  
 nohup python ./PiHome/WebShell/webshell.py --ssl-disable 0 > ./log/pihome-webshell.log 2>&1 & 
